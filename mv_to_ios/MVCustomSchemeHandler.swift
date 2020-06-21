a790ffa322946ff3759e9af16bd3f55455a7c6bd
waffs702 <waffs702>
2020-06-21 16:20:21 +0900

    wkWebView実装

---
+++ mv_to_ios/MVCustomSchemeHandler.swift
@@ -0,0 +1,83 @@
+//
+//  MVCustomSchemeHandler.swift
+//  mv_to_ios
+//
+//  Created by mac-user on 2020/06/21.
+//  Copyright © 2020 developer. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+enum MVCustomSchemeHandlerError: Error {
+    case notFound(filename: String)
+}
+
+class MVCustomSchemeHandler: NSObject, WKURLSchemeHandler {
+    static let MVScheme = "rpgmv"
+    
+    func webView(_ webView: WKWebView, start urlSchemeTask: WKURLSchemeTask) {
+        let url = urlSchemeTask.request.url!
+        let urlString = url.absoluteString
+        let file = urlString.replacingOccurrences(of: "rpgmv:///", with: "")
+        let path = (file as NSString).deletingPathExtension
+        let ext = (file as NSString).pathExtension
+        
+        guard let bundleUrl = Bundle.main.url(forResource: path, withExtension: ext, subdirectory: "htmlSource") else {
+            urlSchemeTask.didFailWithError(MVCustomSchemeHandlerError.notFound(filename: "\(file)"))
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: bundleUrl)
+            let mineType = getMineType(ext: ext)
+            let response = URLResponse(url: url, mimeType: mineType, expectedContentLength: data.count, textEncodingName: "UTF-8")
+            
+            urlSchemeTask.didReceive(response)
+            urlSchemeTask.didReceive(data)
+            urlSchemeTask.didFinish()
+        } catch {
+            urlSchemeTask.didFailWithError(error)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
+    }
+    
+    func getMineType(ext: String) -> String {
+        if (ext == "html" || ext == "HTML") {
+            return "text/html"
+        }
+        if (ext == "css" || ext == "CSS") {
+            return "text/css"
+        }
+        if (ext == "ttf" || ext == "TTF") {
+            return "font/ttf"
+        }
+        if (ext == "otf" || ext == "OTF") {
+            return "font/otf"
+        }
+        if (ext == "js" || ext == "JS") {
+            return "text/javascript"
+        }
+        if (ext == "json" || ext == "JSON") {
+            return "application/json"
+        }
+        if (ext == "png" || ext == "PNG") {
+            return "image/png"
+        }
+        if (ext == "png" || ext == "PNG") {
+            return "image/png"
+        }
+        if (ext == "jpg" || ext == "JPG" || ext == "jpeg" || ext == "JPEG") {
+            return "image/jpeg"
+        }
+        if (ext == "m4a" || ext == "M4A") {
+            return "audio/m4a"
+        }
+        if (ext == "oga" || ext == "OGA") {
+            return "audio/ogg"
+        }
+        return ""
+    }
+}
--- mv_to_ios/ViewController.swift
+++ mv_to_ios/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    private var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.createWebview()
+        let url = URL(string: "rpgmv:///index.html")!
+        self.webView.load(URLRequest(url:url))
+    }
+        
+    //MARK: - WebView
+    
+    func getWebviewFrame() -> CGRect {
+        let screenWidth:CGFloat = view.frame.size.width
+        let screenHeight:CGFloat = view.frame.size.height
+        let window = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+        let topPadding = window!.safeAreaInsets.top
+        let bottomPadding = window!.safeAreaInsets.bottom
+        
+        let rect = CGRect(x: 0, y: topPadding * -1, width: screenWidth, height: screenHeight + topPadding - bottomPadding)
+        
+        return rect
+    }
+    
+    func createWebview() {
+        let config = WKWebViewConfiguration()
+        config.setURLSchemeHandler(MVCustomSchemeHandler(), forURLScheme: "rpgmv")
+        self.webView = WKWebView(frame: getWebviewFrame(), configuration: config)
+        self.view.addSubview(self.webView)
+        self.webView.translatesAutoresizingMaskIntoConstraints = false
+        self.webView.scrollView.bounces = false
+    }
+    
 }
-
