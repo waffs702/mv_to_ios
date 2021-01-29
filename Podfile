d8c31b7d0f56e527e4840e59648bf790379370d8
waffs702 <waffs702>
2021-01-29 22:38:14 +0900

    admob動画広告再生

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'mv_to_ios' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for mv_to_ios
+  pod 'Google-Mobile-Ads-SDK'
+end
--- mv_to_ios/ScriptHandler.swift
+++ mv_to_ios/ScriptHandler.swift
@@ -11,9 +11,14 @@ import WebKit
 
 class ScriptHandler: NSObject, WKScriptMessageHandler {
     private var webView: WKWebView!
+    private var viewController: ViewController!
     
     func setWebView(_webView: WKWebView) {
         webView = _webView
+    }
+    
+    func setViewController(_viewController: ViewController) {
+        viewController = _viewController
     }
     
     func setUserContentController() -> WKUserContentController {
@@ -36,14 +41,51 @@ class ScriptHandler: NSObject, WKScriptMessageHandler {
     }
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        switch message.name {
-        case "debugLog":
-            debugLog(log: message.body as! String)
-        default:
-            print("default")
+        let body: String = message.body as! String
+        let data = body.data(using: .utf8)!
+        do {
+            let params = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            let callbackKey = params?["k"] as? String ?? ""
+            print("userContentController", "message.name", message.name)
+            switch message.name {
+            case "debugLog":
+                debugLog(log: body)
+            case "initAd":
+                initAd()
+            case "loadRewardedAd":
+                loadRewardedAd(callbackKey: callbackKey)
+            case "showRewardedAd":
+                showRewardedAd(callbackKey: callbackKey)
+            default:
+                print("userContentController", "default")
+            }
+            
+        } catch {
+            print("userContentController error", error.localizedDescription)
         }
     }
     
+    func initAd() {
+        Advertisement.initAd()
+    }
+    
+    func loadRewardedAd(callbackKey: String) {
+        Advertisement.shared.loadRewardedAd {
+            self.callbackToJavascript(args: "onLoaded", key: callbackKey)
+        } onFailed: {
+            self.callbackToJavascript(args: "onFailed", key: callbackKey)
+        }
+    }
+    
+    func showRewardedAd(callbackKey: String) {
+        Advertisement.shared.showRewardedAd(viewContorller: self.viewController) {
+            self.callbackToJavascript(args: "onRewarded", key: callbackKey)
+        } onCanceled: {
+            self.callbackToJavascript(args: "onCanceled", key: callbackKey)
+        } onFailed: {
+            self.callbackToJavascript(args: "onFailed", key: callbackKey)
+        }
+    }
     
 }
 
